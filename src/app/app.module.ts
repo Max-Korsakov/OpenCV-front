6dbeca34071d1954fbe4e3f9cd9ed468396b8144
Max Korsakov <Maksim_Karsakou1epam.com>
2021-03-26 10:56:51 +0300

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { VideoComponent } from './video/video.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { VideoComponent } from './video/video.component';
     VideoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -1,17 +1,20 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit } from "@angular/core";
 declare var Module;
 
 @Component({
-  selector: 'app-video',
-  templateUrl: './video.component.html',
-  styleUrls: ['./video.component.scss']
+  selector: "app-video",
+  templateUrl: "./video.component.html",
+  styleUrls: ["./video.component.scss"],
 })
 export class VideoComponent implements AfterViewInit {
   constructor() {}
 
   public streaming: any = false;
   public video;
-  public videoShow = true;
+  public isDetected;
+  public email;
+  public password;
+  public videoShow = false;
   public loading = false;
   public videoWidth: any;
   public videoHeight: any;
@@ -34,37 +37,65 @@ export class VideoComponent implements AfterViewInit {
   public grayMat: any;
   public showResult = false;
   public faceContanerFull = false;
-
+  public singUpState = false;
+  public timer;
+  public enhense = false;
 
   ngAfterViewInit() {
-    this.video = document.getElementById('video');
-    Module._main = this.opencvIsReady();
+    // this.video = document.getElementById("video");
+    // Module._main = this.opencvIsReady();
+    // setTimeout(()=>console.dir(Module), 1000)
   }
 
+  public signUp() {
+    if (this.email && this.password) {
+      this.singUpState = true;
+      if (this.enhense) {
+        setTimeout(() => {
+          this.video = document.getElementById("video");
+          Module._main = this.opencvIsReady();
+        }, 0);
+      }
+
+      console.log("start");
+    }
+  }
+
+  public signOut() {
+    this.singUpState = false;
+    this.email = null;
+    this.password = null;
+  }
+
+  public setSignOut() {}
+
   public startCamera() {
-    if (this.streaming) {return; }
+    if (this.streaming) {
+      return;
+    }
     navigator.mediaDevices
       .getUserMedia({ video: true, audio: false })
       .then(
-        function(s) {
+        function (s) {
           this.stream = s;
           this.video.srcObject = s;
           this.video.play();
         }.bind(this)
       )
       .catch((err) => {
-        console.log('An error occured! ' + err);
+        console.log("An error occured! " + err);
       });
 
     this.video.addEventListener(
-      'canplay',
-      function(ev) {
+      "canplay",
+      function (ev) {
         if (!this.streaming) {
           this.videoWidth = this.video.videoWidth;
           this.videoHeight = this.video.videoHeight;
-          this.video.setAttribute('width', this.videoWidth);
-          this.video.setAttribute('height', this.videoHeight);
+          this.video.setAttribute("width", this.videoWidth);
+          this.video.setAttribute("height", this.videoHeight);
           this.streaming = true;
+          this.startVideoProcessing();
         }
       }.bind(this),
       false
@@ -72,15 +103,15 @@ export class VideoComponent implements AfterViewInit {
   }
 
   public startVideoProcessing() {
-    this.canvasInput = document.createElement('canvas');
+    this.canvasInput = document.createElement("canvas");
     this.canvasInput.width = this.videoWidth;
     this.canvasInput.height = this.videoHeight;
-    this.canvasInputCtx = this.canvasInput.getContext('2d');
+    this.canvasInputCtx = this.canvasInput.getContext("2d");
 
-    this.canvasBuffer = document.createElement('canvas');
-    this.canvasBuffer.width = this.videoWidth;
-    this.canvasBuffer.height = this.videoHeight;
-    this.canvasBufferCtx = this.canvasBuffer.getContext('2d');
+    // this.canvasBuffer = document.createElement('canvas');
+    // this.canvasBuffer.width = this.videoWidth;
+    // this.canvasBuffer.height = this.videoHeight;
+    // this.canvasBufferCtx = this.canvasBuffer.getContext('2d');
 
     this.srcMat = new Module.Mat(
       this.videoHeight,
@@ -94,7 +125,7 @@ export class VideoComponent implements AfterViewInit {
     );
 
     this.faceClassifier = new Module.CascadeClassifier();
-    this.faceClassifier.load('haarcascade_frontalface_default.xml');
+    this.faceClassifier.load("haarcascade_frontalface_default.xml");
     requestAnimationFrame(this.processVideo.bind(this));
   }
 
@@ -113,6 +144,7 @@ export class VideoComponent implements AfterViewInit {
       this.videoHeight
     );
     this.srcMat.data.set(imageData.data);
+    //this.srcMat.data.set(this.video);
     Module.cvtColor(this.srcMat, this.grayMat, Module.COLOR_RGBA2GRAY);
     const faces = [];
     let size;
@@ -128,169 +160,191 @@ export class VideoComponent implements AfterViewInit {
       const face = faceVect.get(i);
       faces.push(new Module.Rect(face.x, face.y, face.width, face.height));
     }
+    this.isDetected = !!faces.length;
+    if (!this.isDetected && !this.timer) {
+      this.timer = setTimeout(() => {
+        this.singUpState = false;
+        this.email = null;
+        this.password = null;
+        this.stream.getTracks()[0].stop();
+        this.streaming = false;
+      }, 2000);
+    } else if (this.isDetected && this.timer) {
+      clearTimeout(this.timer);
+      this.timer = null;
+    }
+
     faceMat.delete();
     faceVect.delete();
 
-    this.canvasOutputCtx.drawImage(
-      this.canvasInput,
-      0,
-      0,
-      this.videoWidth,
-      this.videoHeight
-    );
-    this.drawResults(this.canvasOutputCtx, faces, 'red', size);
-    const container = document.querySelector('.face-container');
-    if (container.childNodes.length < 15) {
-    requestAnimationFrame(this.processVideo.bind(this)); } else {
-      this.videoShow = true;
-      this.faceContanerFull = true;
-    }
+    // this.canvasOutputCtx.drawImage(
+    //   this.canvasInput,
+    //   0,
+    //   0,
+    //   this.videoWidth,
+    //   this.videoHeight
+    // );
+    //this.drawResults(this.canvasOutputCtx, faces, 'red', size);
+    // const container = document.querySelector(".face-container");
+    // if (container.childNodes.length < 1500) {
+    //   requestAnimationFrame(this.processVideo.bind(this));
+    // } else {
+    //   this.videoShow = true;
+    //   this.faceContanerFull = true;
+    // }
+    requestAnimationFrame(this.processVideo.bind(this));
   }
 
+  // public processDocumentPhoto() {
+  //   this.videoShow = false;
+  //   this.canvasInputCtx.drawImage(
+  //     this.video,
+  //     0,
+  //     0,
+  //     this.videoWidth,
+  //     this.videoHeight
+  //   );
+  //   const imageData = this.canvasInputCtx.getImageData(
+  //     0,
+  //     0,
+  //     this.videoWidth,
+  //     this.videoHeight
+  //   );
+  //   this.srcMat.data.set(imageData.data);
+  //   Module.cvtColor(this.srcMat, this.grayMat, Module.COLOR_RGBA2GRAY);
+  //   const faces = [];
+  //   let size;
+  //   const faceVect = new Module.RectVector();
+  //   const faceMat = new Module.Mat();
 
-public processDocumentPhoto() {
-  this.videoShow = false;
-  this.canvasInputCtx.drawImage(
-    this.video,
-    0,
-    0,
-    this.videoWidth,
-    this.videoHeight
-  );
-  const imageData = this.canvasInputCtx.getImageData(
-    0,
-    0,
-    this.videoWidth,
-    this.videoHeight
-  );
-  this.srcMat.data.set(imageData.data);
-  Module.cvtColor(this.srcMat, this.grayMat, Module.COLOR_RGBA2GRAY);
-  const faces = [];
-  let size;
-  const faceVect = new Module.RectVector();
-  const faceMat = new Module.Mat();
+  //   Module.pyrDown(this.grayMat, faceMat);
+  //   Module.pyrDown(faceMat, faceMat);
+  //   size = faceMat.size();
 
-  Module.pyrDown(this.grayMat, faceMat);
-  Module.pyrDown(faceMat, faceMat);
-  size = faceMat.size();
+  //   this.faceClassifier.detectMultiScale(faceMat, faceVect);
+  //   for (let i = 0; i < faceVect.size(); i++) {
+  //     const face = faceVect.get(i);
+  //     faces.push(new Module.Rect(face.x, face.y, face.width, face.height));
+  //   }
+  //   faceMat.delete();
+  //   faceVect.delete();
 
-  this.faceClassifier.detectMultiScale(faceMat, faceVect);
-  for (let i = 0; i < faceVect.size(); i++) {
-    const face = faceVect.get(i);
-    faces.push(new Module.Rect(face.x, face.y, face.width, face.height));
-  }
-  faceMat.delete();
-  faceVect.delete();
+  //   this.canvasOutputCtx.drawImage(
+  //     this.canvasInput,
+  //     0,
+  //     0,
+  //     this.videoWidth,
+  //     this.videoHeight
+  //   );
+  //   //this.drawResults(this.canvasOutputCtx, faces, 'red', size);
+  //   const container = document.querySelector('.document-container');
+  //   if (container.childNodes.length < 4) {
+  //   requestAnimationFrame(this.processDocumentPhoto.bind(this)); } else {
+  //     this.videoShow = true;
+  //   }
+  // }
 
-  this.canvasOutputCtx.drawImage(
-    this.canvasInput,
-    0,
-    0,
-    this.videoWidth,
-    this.videoHeight
-  );
-  this.drawResults(this.canvasOutputCtx, faces, 'red', size);
-  const container = document.querySelector('.document-container');
-  if (container.childNodes.length < 4) {
-  requestAnimationFrame(this.processDocumentPhoto.bind(this)); } else {
-    this.videoShow = true;
-  }
-}
+  // public drawResults(ctx, results, color, size) {
+  //   for (let i = 0; i < results.length; ++i) {
+  //     const rect = results[i];
+  //     const xRatio = this.videoWidth / size.width;
+  //     const yRatio = this.videoHeight / size.height;
+  //     const face = {} as any;
+  //     face.x = rect.x * xRatio;
+  //     face.y = rect.y * yRatio;
+  //     face.width = rect.width * xRatio;
+  //     face.height = rect.height * yRatio;
+  //     let container;
+  //     if (!this.faceContanerFull) {
+  //        container = document.querySelector('.face-container');
+  //     } else {
+  //        container = document.querySelector('.document-container');
+  //     }
+  //     if (this.picktureInterval && container.childNodes.length < 15) {
+  //       this.picktureInterval = false;
+  //       this.getPicture(face, this.canvasInputCtx);
+  //       setTimeout(() => {
+  //         this.picktureInterval = true;
+  //       }, 300);
+  //     }
+  //     ctx.lineWidth = 3;
+  //     ctx.strokeStyle = color;
+  //     ctx.strokeRect(
+  //       rect.x * xRatio,
+  //       rect.y * yRatio,
+  //       rect.width * xRatio,
+  //       rect.height * yRatio
+  //     );
+  //   }
+  // }
 
+  // public checkPhoto() {
+  //   this.loading = true;
+  //   setTimeout(() => {
+  //     this.loading = false;
+  //     this.showResult = !this.showResult;
+  //   }, 1200);
+  // }
 
-  public drawResults(ctx, results, color, size) {
-    for (let i = 0; i < results.length; ++i) {
-      const rect = results[i];
-      const xRatio = this.videoWidth / size.width;
-      const yRatio = this.videoHeight / size.height;
-      const face = {} as any;
-      face.x = rect.x * xRatio;
-      face.y = rect.y * yRatio;
-      face.width = rect.width * xRatio;
-      face.height = rect.height * yRatio;
-      let container;
-      if (!this.faceContanerFull) {
-         container = document.querySelector('.face-container');
-      } else {
-         container = document.querySelector('.document-container');
-      }
-      if (this.picktureInterval && container.childNodes.length < 15) {
-        this.picktureInterval = false;
-        this.getPicture(face, this.canvasInputCtx);
-        setTimeout(() => {
-          this.picktureInterval = true;
-        }, 300);
-      }
-      ctx.lineWidth = 3;
-      ctx.strokeStyle = color;
-      ctx.strokeRect(
-        rect.x * xRatio,
-        rect.y * yRatio,
-        rect.width * xRatio,
-        rect.height * yRatio
-      );
-    }
-  }
+  // public getPicture(imageData, ctx) {
+  //   const ImageData = ctx.getImageData(
+  //     imageData.x,
+  //     imageData.y,
+  //     imageData.height,
+  //     imageData.width
+  //   );
+  //   const MyImage = new Image();
+  //   MyImage.src = getImageURL(ImageData, imageData.width, imageData.height);
+  //   MyImage.className = 'face-image';
+  //   let container;
+  //   if (!this.faceContanerFull) {
+  //      container = document.querySelector('.face-container');
+  //   } else {
+  //      container = document.querySelector('.document-container');
+  //   }
+  //   container.append(MyImage);
 
-  public checkPhoto() {
-    this.loading = true;
-    setTimeout(() => {
-      this.loading = false;
-      this.showResult = !this.showResult;
-    }, 1200);
-  }
-
-  public getPicture(imageData, ctx) {
-    const ImageData = ctx.getImageData(
-      imageData.x,
-      imageData.y,
-      imageData.height,
-      imageData.width
-    );
-    const MyImage = new Image();
-    MyImage.src = getImageURL(ImageData, imageData.width, imageData.height);
-    MyImage.className = 'face-image';
-    let container;
-    if (!this.faceContanerFull) {
-       container = document.querySelector('.face-container');
-    } else {
-       container = document.querySelector('.document-container');
-    }
-    container.append(MyImage);
-
-    function getImageURL(imgData, width, height) {
-      const canvas = document.createElement('canvas');
-      const ctx = canvas.getContext('2d');
-      canvas.width = width;
-      canvas.height = height;
-      ctx.putImageData(imgData, 0, 0);
-      return canvas.toDataURL();
-    }
-  }
+  //   function getImageURL(imgData, width, height) {
+  //     const canvas = document.createElement('canvas');
+  //     const ctx = canvas.getContext('2d');
+  //     canvas.width = width;
+  //     canvas.height = height;
+  //     ctx.putImageData(imgData, 0, 0);
+  //     return canvas.toDataURL();
+  //   }
+  // }
 
   public stopVideoProcessing() {
-    if (this.src != null && !this.src.isDeleted()) { this.src.delete(); }
-    if (this.dstC1 != null && !this.dstC1.isDeleted()) { this.dstC1.delete(); }
-    if (this.dstC3 != null && !this.dstC3.isDeleted()) { this.dstC3.delete(); }
-    if (this.dstC4 != null && !this.dstC4.isDeleted()) { this.dstC4.delete(); }
-  }
-
-  public processFaceCapturing() {
-    if (this.videoShow) {
-      this.videoShow = false;
-      this.canvasOutput = document.getElementById('canvasOutput');
-      this.canvasOutput.width = this.videoWidth;
-      this.canvasOutput.height = this.videoHeight;
-      this.detectFace = document.getElementById('face');
-      this.canvasOutputCtx = this.canvasOutput.getContext('2d');
-      this.startVideoProcessing();
-    } else {
-      this.videoShow = true;
+    if (this.src != null && !this.src.isDeleted()) {
+      this.src.delete();
+    }
+    if (this.dstC1 != null && !this.dstC1.isDeleted()) {
+      this.dstC1.delete();
+    }
+    if (this.dstC3 != null && !this.dstC3.isDeleted()) {
+      this.dstC3.delete();
+    }
+    if (this.dstC4 != null && !this.dstC4.isDeleted()) {
+      this.dstC4.delete();
     }
   }
 
+  // public processFaceCapturing() {
+  //   if (this.videoShow) {
+  //     this.videoShow = false;
+  //     this.canvasOutput = document.getElementById("canvasOutput");
+  //     this.canvasOutput.width = this.videoWidth;
+  //     this.canvasOutput.height = this.videoHeight;
+  //     this.detectFace = document.getElementById("face");
+  //     this.canvasOutputCtx = this.canvasOutput.getContext("2d");
+  //     this.startVideoProcessing();
+  //   } else {
+  //     this.videoShow = true;
+  //   }
+  // }
+
   public opencvIsReady() {
-    console.log('OpenCV.js is ready');
+    // console.log("OpenCV.js is ready");
     this.startCamera();
   }
 }
